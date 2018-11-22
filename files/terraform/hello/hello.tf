cc23d977d029f71d59f5ee221a8daa7f03117f73
Hongkai Liu <hongkailiu@users.noreply.github.com>
2018-11-22 08:44:01 -0500

    Update files/terraform/hello/hello.tf

--- files/terraform/hello/hello.tf
+++ files/terraform/hello/hello.tf
@@ -2,6 +2,14 @@ variable "cluster_name" {
   default = "hongkliu-tf"
 }
 
+variable "ami_id" {
+  default = "ami-0feb1655b7868b845"
+}
+
+variable "instance_type" {
+  default = "m5.xlarge"
+}
+
 provider "aws" {
   access_key = "${var.access_key}"
   secret_key = "${var.secret_key}"
@@ -9,20 +17,33 @@ provider "aws" {
 }
 
 resource "aws_instance" "worker" {
-  ami           = "ami-c6e27cbe"
-  instance_type = "t2.micro"
+  ami           = "${var.ami_id}"
+  instance_type = "${var.instance_type}"
   subnet_id = "subnet-4879292d"
   security_groups = [ "sg-5c5ace38" ]
   key_name = "id_rsa_perf"
   tags = {
     "Name" = "${var.cluster_name}-worker-${count.index + 1}"
   }
+  count = 2
+}
+
+resource "aws_instance" "infra" {
+  ami           = "${var.ami_id}"
+  instance_type = "${var.instance_type}"
+  subnet_id = "subnet-4879292d"
+  security_groups = [ "sg-5c5ace38" ]
+  key_name = "id_rsa_perf"
+  tags = {
+    "Name" = "${var.cluster_name}-infra-${count.index + 1}"
+  }
   count = 1
 }
 
+
 resource "aws_instance" "master" {
-  ami           = "ami-c6e27cbe"
-  instance_type = "t2.micro"
+  ami           = "${var.ami_id}"
+  instance_type = "${var.instance_type}"
   subnet_id = "subnet-4879292d"
   security_groups = [ "sg-5c5ace38" ]
   key_name = "id_rsa_perf"

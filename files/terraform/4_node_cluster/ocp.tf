fb106ea294ed87e369b72e768e65b62b3c49ce96
Hongkai Liu <hongkailiu@users.noreply.github.com>
2018-11-22 13:44:14 -0500

    Add gluster nodes

--- files/terraform/4_node_cluster/ocp.tf
+++ files/terraform/4_node_cluster/ocp.tf
@@ -18,6 +18,10 @@ variable "kubernetes_cluster_value" {
   default = "hongkliu-ocp"
 }
 
+variable "gluster_volume_size" {
+  default = 1000
+}
+
 provider "aws" {
   access_key = "${var.access_key}"
   secret_key = "${var.secret_key}"
@@ -77,4 +81,29 @@ resource "aws_instance" "master" {
     delete_on_termination =  true
   }
   count = 1
+}
+
+resource "aws_instance" "gluster" {
+  ami           = "${var.ami_id}"
+  instance_type = "${var.instance_type}"
+  subnet_id = "subnet-4879292d"
+  security_groups = [ "sg-5c5ace38" ]
+  key_name = "id_rsa_perf"
+  tags = {
+    Name = "${var.cluster_name}-gluster-${count.index + 1}"
+    KubernetesCluster = "${var.kubernetes_cluster_value}"
+  }
+  root_block_device = {
+    volume_size = 23
+    volume_type = "gp2"
+    delete_on_termination =  true
+  }
+
+  ebs_block_device = {
+    device_name = /dev/sdf
+    volume_size = "${var.gluster_volume_size}"
+    volume_type = "gp2"
+    delete_on_termination =  true
+  }
+  count = 0
 }

2cfec2b1137d87e6d0d17d7aefce42d9912329af
Hongkai Liu <hongkailiu@users.noreply.github.com>
2018-11-22 09:16:43 -0500

    Add files/terraform/4_node_cluster

---
+++ files/terraform/4_node_cluster/ocp.tf
@@ -0,0 +1,80 @@
+variable "region" {
+  default = "us-west-2"
+}
+
+variable "cluster_name" {
+  default = "hongkliu-tf"
+}
+
+variable "ami_id" {
+  default = "ami-0feb1655b7868b845"
+}
+
+variable "instance_type" {
+  default = "m5.xlarge"
+}
+
+variable "kubernetes_cluster_value" {
+  default = "hongkliu-ocp"
+}
+
+provider "aws" {
+  access_key = "${var.access_key}"
+  secret_key = "${var.secret_key}"
+  region     = "${var.region}"
+}
+
+resource "aws_instance" "worker" {
+  ami           = "${var.ami_id}"
+  instance_type = "${var.instance_type}"
+  subnet_id = "subnet-4879292d"
+  security_groups = [ "sg-5c5ace38" ]
+  key_name = "id_rsa_perf"
+  tags = {
+    Name = "${var.cluster_name}-worker-${count.index + 1}"
+    KubernetesCluster = "${var.kubernetes_cluster_value}"
+  }
+  root_block_device = {
+      volume_size = 23
+      volume_type = "gp2"
+      delete_on_termination =  true
+    }
+  count = 2
+}
+
+resource "aws_instance" "infra" {
+  ami           = "${var.ami_id}"
+  instance_type = "${var.instance_type}"
+  subnet_id = "subnet-4879292d"
+  security_groups = [ "sg-5c5ace38" ]
+  key_name = "id_rsa_perf"
+  tags = {
+    Name = "${var.cluster_name}-infra-${count.index + 1}"
+    KubernetesCluster = "${var.kubernetes_cluster_value}"
+  }
+  root_block_device = {
+      volume_size = 23
+      volume_type = "gp2"
+      delete_on_termination =  true
+    }
+  count = 1
+}
+
+
+resource "aws_instance" "master" {
+  ami           = "${var.ami_id}"
+  instance_type = "${var.instance_type}"
+  subnet_id = "subnet-4879292d"
+  security_groups = [ "sg-5c5ace38" ]
+  key_name = "id_rsa_perf"
+  tags = {
+    Name = "${var.cluster_name}-master-${count.index + 1}"
+    KubernetesCluster = "${var.kubernetes_cluster_value}"
+  }
+  root_block_device = {
+    volume_size = 23
+    volume_type = "gp2"
+    delete_on_termination =  true
+  }
+  count = 1
+}

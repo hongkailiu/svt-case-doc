81984c5a1e27055692cb6133e8a9a74429847a3d
Hongkai Liu <hongkailiu@users.noreply.github.com>
2018-12-04 17:17:32 -0500

    Update scripts/tf_flexy_jenkins.sh

--- scripts/tf_flexy_jenkins.sh
+++ scripts/tf_flexy_jenkins.sh
@@ -4,12 +4,6 @@ set -o errexit
 set -o nounset
 set -o pipefail
 
-### assume
-### 1. this script is at /home/fedora and ${GOPATH}=/home/fedora/go and perf key is /home/fedora/id_rsa_perf
-### 2. ~/Downloads/secret.sh has the inv vars required by the playbook
-### 3. /home/fedora/svt-case-doc (with valid secret.tfvars)
-
-
 readonly TF_BIN=/data/jenkins_home/my-tool/terraform
 ${TF_BIN} --version
 ${TF_BIN} init ./svt-case-doc/files/terraform/4_node_cluster/
@@ -28,8 +22,8 @@ git -C ./openshift-ansible checkout release-3.11
 
 readonly OCPTF_DIR=/data/jenkins_home/my-tool/ocptf
 ${OCPTF_DIR}/build/ocptf --list
-ansible-playbook -i ${OCPTF_DIR}/build/ocptf -i "${OCPTF_DIR}/test_files/ocpft/inv/2.file" "${OCPTF_DIR}/test_files/ocpft/playbook/test.yaml" --extra-vars "ansible_ssh_private_key_file=/data/secret/id_rsa_perf"
+ansible-playbook -i "${OCPTF_DIR}/build/ocptf" -i "${OCPTF_DIR}/test_files/ocpft/inv/2.file" "${OCPTF_DIR}/test_files/ocpft/playbook/test.yaml" --extra-vars "ansible_ssh_private_key_file=/data/secret/id_rsa_perf"
 
 source /data/secret/secret.sh
-#ansible-playbook -i build/ocptf -i test_files/ocpft/inv/2.file ~/openshift-ansible/playbooks/prerequisites.yml  --extra-vars "ansible_ssh_private_key_file=/data/secret/id_rsa_perf"
-#ansible-playbook -i build/ocptf -i test_files/ocpft/inv/2.file ~/openshift-ansible/playbooks/deploy_cluster.yml --extra-vars "ansible_ssh_private_key_file=/data/secret/id_rsa_perf"
+ansible-playbook -i "${OCPTF_DIR}/build/ocptf" -i "${OCPTF_DIR}/test_files/ocpft/inv/2.file" ./openshift-ansible/playbooks/prerequisites.yml  --extra-vars "ansible_ssh_private_key_file=/data/secret/id_rsa_perf"
+ansible-playbook -i "${OCPTF_DIR}/build/ocptf" -i "${OCPTF_DIR}/test_files/ocpft/inv/2.file" ./openshift-ansible/playbooks/deploy_cluster.yml --extra-vars "ansible_ssh_private_key_file=/data/secret/id_rsa_perf"

4a2b50dec953a8b9897fae18494d899ada85288f
Hongkai Liu <hongkailiu@users.noreply.github.com>
2019-01-29 15:48:35 -0500

    Update my_installer_post.sh

--- scripts/my_installer_post.sh
+++ scripts/my_installer_post.sh
@@ -2,6 +2,9 @@
 
 ### create a jump node inside cluster VPC
 ### with the same security group and subnet
+### the jump node uses ami-0f7e779f5a384f9fc (clean fedora 29)
+### ami-0f7e779f5a384f9fc is at us-east-2
+### so your aws account has to set the default profile to that region for this script to work
 
 set -o errexit
 set -o nounset

f7cb609a857c1ef04fd2a9d9cc12d20c0821ffd8
Hongkai Liu <hongkailiu@users.noreply.github.com>
2019-01-30 10:30:19 -0500

    Update my_installer.sh

--- scripts/my_installer.sh
+++ scripts/my_installer.sh
@@ -12,7 +12,6 @@
 
 # curl -LO https://raw.githubusercontent.com/hongkailiu/svt-case-doc/master/scripts/my_installer.sh
 # bash ./my_installer.sh <4.0.0-0.nightly-2019-01-29-025207>
-# mkdir 
 
 set -o errexit
 set -o nounset

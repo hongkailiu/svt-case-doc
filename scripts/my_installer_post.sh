bc105704214eda69b9c0fc45ee2e16f758f0a46a
Hongkai Liu <hongkailiu@users.noreply.github.com>
2019-03-07 15:03:45 -0500

    Update scripts/my_installer_post.sh

--- scripts/my_installer_post.sh
+++ scripts/my_installer_post.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-### create a jump node inside cluster VPC
-### with the same security group and subnet
+### create a jump node: implementation of the following link
+### https://github.com/openshift/training/blob/master/docs/03-explore.md#exploring-rhel-coreos
 ### the jump node uses `ami-0f7e779f5a384f9fc` (clean fedora 29) which is in US East (Ohio), ie, us-east-2
 ### 1. Your aws account has to set the default profile to REGION us-east-2 for this script to work
 ### 2. `pip install awscli` if not yet
 ### 3. It is ASSSUMED that the libra key is used for 4.0 OCP cluster
 ####################################################################################
-### REMEMBER to terminate this instance of jump node BEFORE destroy your cluster
+### REMEMBER to terminate this instance of jump node and delete the created security group BEFORE destroy your cluster
 
 ### curl -LO https://raw.githubusercontent.com/hongkailiu/svt-case-doc/master/scripts/my_installer_post.sh
 ### bash ./my_installer_post.sh
@@ -22,22 +22,39 @@ readonly KERBEROS_ID='hongkliu'
 readonly CLUSTER_NAME='hongkliu1'
 readonly PRIVATE_KEY="${HOME}/.ssh/libra.pem"
 
+bye () {
+	# shellcheck disable=SC2059
+	printf "${@}" >&2
+	exit 1
+}
+
 echo "creating cluster info ..."
-readonly MASTER_PRIVATE_IP=$(oc get node | grep master | head -n 1 | awk '{print $1}')
+MASTER_PRIVATE_IP="$(oc get node | grep master | head -n 1 | awk '{print $1}')" || bye 'failed to resolve MASTER_PRIVATE_IP\n'
 echo "MASTER_PRIVATE_IP: ${MASTER_PRIVATE_IP}"
+
+RANDOM_ID="$(oc describe node $(oc get node --no-headers | head -n 1 | awk '{print $1}') | grep "${CLUSTER_NAME}" | awk '{print $3}' | awk -F'/' '{print $2}' |  awk -F'-' '{print $(NF-2)}')" || bye 'failed to resolve RANDOM_ID\n'
+echo "RANDOM_ID: ${RANDOM_ID}"
 
 #readonly SUBNET_ID=$(aws ec2 describe-instances --output json --filters "Name=private-dns-name,Values=${MASTER_PRIVATE_IP}"  | jq -r '.Reservations[].Instances[].SubnetId')
 #readonly SUBNET_ID=$(aws ec2 describe-subnets --output json --filters "Name=tag:kubernetes.io/cluster/${CLUSTER_NAME},Values=owned" "Name=cidrBlock,Values=10.0.0.0/20" | jq -r '.Subnets[].SubnetId' | head -n 1)
-readonly SUBNET_ID=$(aws ec2 describe-subnets --output json --filters "Name=tag:kubernetes.io/cluster/${CLUSTER_NAME},Values=owned" | jq -r --arg key "${CLUSTER_NAME}-public" '.Subnets[] | select((.Tags[].Value | contains($key)) and (.Tags[].Value | endswith("a"))) | .SubnetId')
+SUBNET_ID="$(aws ec2 describe-subnets --output json --filters "Name=tag:kubernetes.io/cluster/${CLUSTER_NAME}-${RANDOM_ID},Values=owned" | jq -r --arg key "${CLUSTER_NAME}-${RANDOM_ID}-public" '.Subnets[] | select((.Tags[].Value | contains($key)) and (.Tags[].Value | endswith("a"))) | .SubnetId')" || bye 'failed to resolve SUBNET_ID\n'
 echo "SUBNET_ID: ${SUBNET_ID}"
 
-readonly SECURITY_GROUP_ID=$(aws ec2 describe-instances --output json --filters "Name=private-dns-name,Values=${MASTER_PRIVATE_IP}"  | jq -r '.Reservations[].Instances[].SecurityGroups[].GroupId')
+VCP_ID="$(aws ec2 describe-vpcs --filters "Name=tag:kubernetes.io/cluster/${CLUSTER_NAME}-${RANDOM_ID},Values=owned" | jq -r .Vpcs[0].VpcId)" || bye 'failed to resolve VCP_ID\n'
+echo "VCP_ID: ${VCP_ID}"
+
+echo "creating a sg ..."
+#readonly SECURITY_GROUP_ID=$(aws ec2 describe-instances --output json --filters "Name=private-dns-name,Values=${MASTER_PRIVATE_IP}"  | jq -r '.Reservations[].Instances[].SecurityGroups[].GroupId')
+SECURITY_GROUP_ID="$(aws ec2 create-security-group --group-name "qe-${KERBEROS_ID}-${CLUSTER_NAME}-${RANDOM_ID}-ocp" --description "created by my_installer_post.sh" --vpc-id "${VCP_ID}" --query 'GroupId' | grep "sg-" | tr -d '"' | tr -d " ")" || bye 'failed to resolve SECURITY_GROUP_ID\n'
 echo "SECURITY_GROUP_ID: ${SECURITY_GROUP_ID}"
 
+echo "opening ssh port (22) in the sg ..."
+aws ec2 authorize-security-group-ingress --group-id ${SECURITY_GROUP_ID} --protocol tcp --port 22 --cidr 0.0.0.0/0
+
 echo "creating a jump node ..."
-readonly INSTANCE_ID=$(aws ec2 run-instances --image-id ami-0f7e779f5a384f9fc --security-group-ids "${SECURITY_GROUP_ID}" --count 1    --associate-public-ip-address \
+INSTANCE_ID="$(aws ec2 run-instances --image-id ami-0f7e779f5a384f9fc --security-group-ids "${SECURITY_GROUP_ID}" --count 1    --associate-public-ip-address \
       --instance-type m5.xlarge --subnet "${SUBNET_ID}"  --key-name libra  --query 'Instances[*].InstanceId'  \
-      --tag-specifications="[{\"ResourceType\":\"instance\",\"Tags\":[{\"Key\":\"Name\",\"Value\":\"qe-${KERBEROS_ID}-${CLUSTER_NAME}-ocp-jn\"}]}]" | grep "i-" | tr -d '"' | tr -d " ")
+      --tag-specifications="[{\"ResourceType\":\"instance\",\"Tags\":[{\"Key\":\"Name\",\"Value\":\"qe-${KERBEROS_ID}-${CLUSTER_NAME}-${RANDOM_ID}-ocp-jn\"}]}]" | grep "i-" | tr -d '"' | tr -d " ")" || bye 'failed to resolve INSTANCE_ID\n'
 echo "INSTANCE_ID: ${INSTANCE_ID}" 
 
 echo "sleep 20 secs, waiting for the host is ready"

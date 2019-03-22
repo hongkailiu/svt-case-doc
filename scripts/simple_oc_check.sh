e2ddc031567ddd031445a9022d505b05842be77d
Hongkai Liu <hongkailiu@users.noreply.github.com>
2019-03-22 08:36:54 -0400

    Update scripts/simple_oc_check.sh

--- scripts/simple_oc_check.sh
+++ scripts/simple_oc_check.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 
-### $ bash -x ./test.sh 2>&1 | tee -a test.log
+### create the bc used in the test
+# $ oc new-project testproject
+### Create an app
+# $ oc new-app https://github.com/sclorg/cakephp-ex
+
+### download and start the script
+### curl -O https://raw.githubusercontent.com/hongkailiu/svt-case-doc/master/scripts/simple_oc_check.sh
+### $ bash -x ./simple_oc_check.sh 2>&1 | tee -a test.log
 
 set -o errexit
 set -o nounset

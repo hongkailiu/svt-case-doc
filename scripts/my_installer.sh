1ad95ff7ead1783d6f9c9cc938f3249c89777047
Hongkai Liu <hongkailiu@users.noreply.github.com>
2019-04-03 10:57:54 -0400

    Update scripts/my_installer.sh

--- scripts/my_installer.sh
+++ scripts/my_installer.sh
@@ -23,6 +23,8 @@
 
 # curl -LO https://raw.githubusercontent.com/hongkailiu/svt-case-doc/master/scripts/my_installer.sh
 # bash ./my_installer.sh <4.0.0-0.nightly-2019-01-29-025207>
+### OR
+# bash ./my_installer.sh <quay.io/openshift-release-dev/ocp-release:4.0.0-0.10>
 
 set -o errexit
 set -o nounset
@@ -33,22 +35,29 @@ if [ "$#" -ne 1 ]; then
     exit 1
 fi
 
+if [[ $1 == *":"* ]]; then
+  echo "$1 contains :, use it as release image"
+  release_image=$1
+else
+  echo "$1 contains no :, use it as release image tag"
+  release_image="registry.svc.ci.openshift.org/ocp/release:$1"
+fi
+
 INSTALL_FOLDER=$(date '+%Y%m%d_%H%M%S')
 mkdir ${INSTALL_FOLDER}
 echo "INSTALL_FOLDER: ${INSTALL_FOLDER}"
+echo "release_image: ${release_image}"
+echo "release_image: ${release_image}" >> "${INSTALL_FOLDER}/version.txt"
 
-readonly BUILD_TAG=$1
-readonly IMAGE=$(oc adm release info --image-for=installer "registry.svc.ci.openshift.org/ocp/release:${BUILD_TAG}")
+installer_image=$(oc adm release info --image-for=installer "${release_image}")
+echo "INSTALLER_IMAGE: ${installer_image}" >> "${INSTALL_FOLDER}/version.txt"
+echo "using the installer bin from IMAGE: ${installer_image}"
+
 ### the following command will overwrite ./openshift-install
-oc image extract ${IMAGE} --file  /usr/bin/openshift-install
+oc image extract ${installer_image} --file  /usr/bin/openshift-install
 mv openshift-install "${INSTALL_FOLDER}/"
 chmod +x "${INSTALL_FOLDER}/openshift-install"
-echo "${BUILD_TAG}" >> "${INSTALL_FOLDER}/version.txt"
 
-#readonly ID=$(docker create ${IMAGE})
-#docker cp ${ID}:/usr/bin/openshift-install "./${INSTALL_FOLDER}/"
-#docker rm ${ID}
-echo "using the installer bin from IMAGE: ${IMAGE}"
 echo "installer version: $(./${INSTALL_FOLDER}/openshift-install version)"
 
 readonly INSTALL_CONFIG="${HOME}/install-config.yaml"
@@ -56,8 +65,8 @@ readonly INSTALL_CONFIG="${HOME}/install-config.yaml"
 if [[ ! -f "${INSTALL_CONFIG}" ]]; then
   echo "File not found: ${INSTALL_CONFIG}"
   echo "Continue installation with the following commands:"
-  echo "export OPENSHIFT_INSTALL_RELEASE_IMAGE_OVERRIDE=registry.svc.ci.openshift.org/ocp/release:${BUILD_TAG}"
-  echo "export _OPENSHIFT_INSTALL_RELEASE_IMAGE_OVERRIDE=registry.svc.ci.openshift.org/ocp/release:${BUILD_TAG}"
+  echo "export OPENSHIFT_INSTALL_RELEASE_IMAGE_OVERRIDE=${release_image}"
+  echo "export _OPENSHIFT_INSTALL_RELEASE_IMAGE_OVERRIDE=${release_image}"
   echo "./${INSTALL_FOLDER}/openshift-install create install-config --dir=./${INSTALL_FOLDER}"
   echo "vi ./${INSTALL_FOLDER}/install-config.yaml"
   echo "./${INSTALL_FOLDER}/openshift-install create cluster --dir=./${INSTALL_FOLDER}"
@@ -65,8 +74,8 @@ if [[ ! -f "${INSTALL_CONFIG}" ]]; then
 else
   echo "found: ${INSTALL_CONFIG}, using it for installation ..."
   cp -v "${INSTALL_CONFIG}" ./"${INSTALL_FOLDER}"/
-  export OPENSHIFT_INSTALL_RELEASE_IMAGE_OVERRIDE=registry.svc.ci.openshift.org/ocp/release:${BUILD_TAG}
-  export _OPENSHIFT_INSTALL_RELEASE_IMAGE_OVERRIDE=registry.svc.ci.openshift.org/ocp/release:${BUILD_TAG}
+  export OPENSHIFT_INSTALL_RELEASE_IMAGE_OVERRIDE=${release_image}
+  export _OPENSHIFT_INSTALL_RELEASE_IMAGE_OVERRIDE=${release_image}
   ./${INSTALL_FOLDER}/openshift-install create cluster --dir=./${INSTALL_FOLDER}
 fi
 

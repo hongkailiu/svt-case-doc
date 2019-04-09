da4f54f8125df8ae9d00a7749bcf7856a01d6529
Hongkai Liu <hongkailiu@users.noreply.github.com>
2019-04-09 16:36:52 -0400

    Update scripts/simple_1689061.sh

--- scripts/simple_1689061.sh
+++ scripts/simple_1689061.sh
@@ -19,12 +19,15 @@ running_build_node=ip-10-0-141-143.us-east-2.compute.internal
 standby_build_node=ip-10-0-149-124.us-east-2.compute.internal
 
 oc adm cordon ${standby_build_node}
+sleep 10
 oc start-build -n testproject django-ex
 oc get pod -o wide -n testproject
 oc get build -n testproject
+sleep 10
 oc adm uncordon ${standby_build_node}
-ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -n core@${running_build_node} "sudo dd if=/dev/urandom of=file$(date +"%s").txt bs=1048576 count=1024"
+ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -n core@${running_build_node} "sudo dd if=/dev/zero of=file$(date +"%s").txt bs=1048576 count=1024"
 ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -n core@${running_build_node} "sudo df -h"
+ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -n core@${running_build_node} "sudo ls -al -h"
 oc get pod -o wide -n testproject
 oc get build -n testproject
 
